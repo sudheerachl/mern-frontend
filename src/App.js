6f7b4f94ecd64124055f59a69cc88f0b2fae698e
UniKishore <143478258+UniKishore@users.noreply.github.com>
2023-11-19 13:42:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Home from './components/home';
+import Blogs from './components/blogs';
+import HealthProblems from './components/healthproblems';
+import Healthbot from './components/healthbot';
+import Signin from './components/signin';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Nave } from './components/dashboard';
+import Testing from './components/chats';
+import HealthProblems1 from './components/healthproblems1';
+import Profile from './components/profile';
+import Quiz2 from './components/quiz2';
+
+function App() {
+  return (
+    <>
+      <Router>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/healthproblems' element={<HealthProblems/>} />
+          <Route path='/healthbot' element={<Healthbot />} />
+          
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/dashboard' element={<Nave />} />
+          <Route path='/chats' element={<Testing />} />
+          <Route path='/healthproblems1' element={<HealthProblems1 />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/quiz2' element={<Quiz2 />} />
+          
+        </Routes>
+
+        
+        
+      </Router>
+    </>
+  );
+}
+
+export default App;
