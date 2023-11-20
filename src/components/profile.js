018e97d4643195d0828366d59c070a3f08930220
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-21 00:07:03 +0900

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Navbar1 from './navbar1'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar1 from './Navbar1';
+import VerticalNavbar from './VerticalNavbar';
+import Successdoctor from './Successdoctor';
+import Infodoctor from './Infodoctor';
 
-function Profile() {
+const ProfilePage = () => {
+  const [selectedOption, setSelectedOption] = useState('/info-doctor');
+
   return (
-    <div>
+    <Router>
+      <div className="profile-page">
         <Navbar1 />
+        <VerticalNavbar />
+        <div className="main-content">
+          {/* Content based on the selected option */}
+          <Route path='/info-doctor' component={Infodoctor} />
+          <Route path='/delete-doctor' component={DeleteDoctor} />
+          <Route path='/update-doctor' component={UpdateDoctor} />
+          <Route path='/' component={Logout} />
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Profile
+export default ProfilePage;
