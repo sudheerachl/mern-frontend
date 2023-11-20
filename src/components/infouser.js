67a100bd1d5161b4283f173c19110f4786704593
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-21 00:10:31 +0900

    Update and rename info.js to infouser.js

--- src/components/infouser.js
+++ src/components/infouser.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-const Infodoctor = () => {
+const Infouser = () => {
   const [userDetails, setUserDetails] = useState(null);
   const username = localStorage.getItem('username');
 
@@ -36,4 +36,4 @@ useEffect(() => {
   );
 };
 
-export default Infodoctor;
+export default Infouser;
