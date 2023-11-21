7515147e89a406172fe9c9a546c17c64dc4885db
gurusaiteja <124774669+gurusaiteja@users.noreply.github.com>
2023-11-21 14:40:58 +0900

    Update signin.js

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import Navbar from './navbar';
-import './Cards.css';
 
 const Signin = () => {
   return (
-    <div className="cards-container">
-      <Navbar />
-      <div className="card">
-        <h1>Empower others as a compassionate doctor</h1>
-        <p>Are you a licensed therapist or a counselor seeking to make a positive impact on the lives of others? Join our platform and connect with individuals in need of your expertise and support. As a doctor, you can provide a safe and confidential space for individuals to share their experiences, develop coping mechanisms, and find renewed hope.</p>
-        <a href="/signindoctor" className="button">Join now</a>
-      </div>
-      <div className="card">
-        <h1>Share your experiences and find connection.</h1>
-        <p>Join a supportive community of individuals who understand what you're going through. In this open and non-judgmental environment, you can express your feelings, share your experiences, and find comfort in knowing that you're not alone. Together, you can uplift each other and find strength in shared understanding.</p>
-        <a href="/signinuser" className="button">Join now</a>
-      </div>
-    </div>
+      <>
+    <div>
+    <Navbar />
+  
+       
+        <div className="main">
+          <div className="heading">
+            <h2>Signup</h2>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-4 py-3 py-sm-0">
+                <div className="card card-style">
+                   <div className="card-body">
+                    <h5 className="card-title">Empower others as a compassionate doctor</h5>
+                    <p className="card-text"> Are you a licensed therapist or a counselor seeking to make a positive impact on the lives of others? </p>
+                    <a href="/signupdoctor" className="btn btn-primary">Resume your journey</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-4 py-3 py-sm-0">
+                <div className="card card-style">
+                 
+                  <div className="card-body">
+                    <h5 className="card-title">Share your experiences and find connection.</h5>
+                    <p className="card-text">  Join a supportive community of individuals who understand what you're going through.</p>
+                   <a href="/signupuser"  className="btn btn-primary">Resume your journey</a>
+                  </div>
+                </div>
+              </div>
+               </div>
+          </div>
+        </div>
+            </div>
+       </>
   );
 };
 
 export default Signin;
+
+
+
+
+
