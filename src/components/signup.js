623353ce6836878dde9374a70c246bd596ffe15d
gurusaiteja <124774669+gurusaiteja@users.noreply.github.com>
2023-11-21 14:33:50 +0900

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,50 +2,56 @@ import React from 'react';
 import Navbar from './navbar';
 
 const Signup = () => {
-  const buttonStyle = {
-    display: 'inline-block',
-    padding: '10px 20px',
-    border: 'none',
-    cursor: 'pointer',
-    backgroundColor: '#007bff',
-    color: '#fff',
-    borderRadius: '5px',
-    textDecoration: 'none',
-  };
   return (
+      <>
     <div>
     <Navbar />
-    <div className="container">
-      
-       <div className="card card-style">
-        <h1>Empower others as a compassionate doctor</h1>
-        <p>
-          Are you a licensed therapist or a counselor seeking to make a
+  
+       
+        <div className="main">
+          <div className="heading">
+            <h2>Blogs</h2>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-4 py-3 py-sm-0">
+                <div className="card card-style">
+                   <div className="card-body">
+                    <h5 className="card-title">Empower others as a compassionate doctor</h5>
+                    <p className="card-text"> Are you a licensed therapist or a counselor seeking to make a
           positive impact on the lives of others? Join our platform and connect
           with individuals in need of your expertise and support. As a doctor,
           you can provide a safe and confidential space for individuals to share
-          their experiences, develop coping mechanisms, and find renewed hope.
-        </p>
-        <a href="/signupdoctor" className="button" style={buttonStyle}>
-          Join now
-        </a>
-      </div>
-      <div className="card card-style">
-        <h1>Share your experiences and find connection.</h1>
-        <p>
-          Join a supportive community of individuals who understand what you're
+          their experiences, develop coping mechanisms, and find renewed hope.</p>
+                    <a href="/signupdoctor" className="btn btn-primary">Join now</>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-4 py-3 py-sm-0">
+                <div className="card card-style">
+                 
+                  <div className="card-body">
+                    <h5 className="card-title">Share your experiences and find connection.</h5>
+                    <p className="card-text">  Join a supportive community of individuals who understand what you're
           going through. In this open and non-judgmental environment, you can
           express your feelings, share your experiences, and find comfort in
           knowing that you're not alone. Together, you can uplift each other and
-          find strength in shared understanding.
-        </p>
-       <a href="/signupuser" className="button" style={buttonStyle}>
-          Join now
-        </a>
-      </div>
-    </div>
-   </div>
+          find strength in shared understanding.</p>
+                   <a href="/signupuser"  className="btn btn-primary">Read More</Link>
+                  </div>
+                </div>
+              </div>
+               </div>
+          </div>
+        </div>
+            </div>
+       </>
   );
 };
 
 export default Signup;
+
+
+
+
+
