0608c27c6f778cdbc25fb55b2b725b92954e0bf0
gurusaiteja <124774669+gurusaiteja@users.noreply.github.com>
2023-11-21 13:03:01 +0900

    Create profiledoctor.js

---
+++ src/components/profiledoctor.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import Navbar1 from './navbar1';
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+const buttonStyle = {
+  margin: '10px',
+  padding: '10px',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  backgroundColor: '#4CAF50',
+  color: 'white',
+  borderRadius: '5px',
+  textDecoration: 'none',
+  display: 'inline-block',
+  textAlign: 'center',
+};
+
+const Profiledoctor = () => {
+  const navigate = useNavigate();
+  const [showContent, setShowContent] = useState('loginSuccess'); // State to manage content visibility
+
+  const handleContentChange = (content) => {
+    setShowContent(content);
+  };
+
+  const renderContent = () => {
+    switch (showContent) {
+      case 'loginSuccess':
+        return (
+          <div>
+            <h1>Login Success Page</h1>
+          </div>
+        );
+      case 'deleteUser':
+        return (
+          <div>
+            <h1>Delete Doctor</h1>
+          </div>
+        );
+      case 'updateUser':
+        return (
+          <div>
+            <h1>Edit Doctor</h1>
+          </div>
+        );
+      case 'infoUser':
+        return (
+          <div>
+            <h1>View Doctor Info</h1>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="profile-page">
+      <Navbar1 />
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <Link to='/' onClick={() => handleContentChange('loginSuccess')} style={buttonStyle}>
+          Logout
+        </Link>
+        <Link to='/deleteuser' onClick={() => handleContentChange('deleteUser')} style={buttonStyle}>
+          Delete Doctor
+        </Link>
+        <Link to='/updateuser' onClick={() => handleContentChange('updateUser')} style={buttonStyle}>
+          Edit Doctor
+        </Link>
+        <Link to='/infouser' onClick={() => handleContentChange('infoUser')} style={buttonStyle}>
+          View Doctor Info
+        </Link>
+      </div>
+      {renderContent()}
+    </div>
+  );
+};
+
+export default Profiledoctor;
