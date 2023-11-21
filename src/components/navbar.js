b05c1695a616c05ae3dbf856e5eb1f66217a74b2
gurusaiteja <124774669+gurusaiteja@users.noreply.github.com>
2023-11-21 14:51:40 +0900

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,10 +27,14 @@ const Navbar = () => {
               <li className="nav-item">
                 <Link className="nav-link" to="/quiz2" aria-label="Navigate to Quiz page">Quiz</Link>
               </li>
+               <li className="nav-item">
+                <Link className="btn btn-outline-primary" to="/signin" role="button" aria-label="Sign in">Sign In</Link>
+               </li>
+               <li className="nav-item">
+                <Link className="btn btn-outline-primary" to="/signup" role="button" aria-label="Sign up">Sign Up</Link>
+               </li>
             </ul>
-
-            <Link className="btn btn-outline-primary" to="/signin" role="button" aria-label="Sign in">Sign In</Link>
-            <Link className="btn btn-outline-primary" to="/signup" role="button" aria-label="Sign up">Sign Up</Link>
+           
           </div>
         </div>
       </nav>
