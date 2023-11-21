d651a4c137eeba902f9689cde08d1331ce1eadb8
gurusaiteja <124774669+gurusaiteja@users.noreply.github.com>
2023-11-21 15:18:22 +0900

    Update profileuser.js

--- src/components/profileuser.js
+++ src/components/profileuser.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar1 from './navbar1';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const buttonStyle = {
@@ -19,37 +20,58 @@ const buttonStyle = {
 const ProfilePage = () => {
   const navigate = useNavigate();
   const [showContent, setShowContent] = useState('loginSuccess'); // State to manage content visibility
+  const [userDetails, setUserDetails] = useState(null); // State to store user details
+  const username = localStorage.getItem('username');
 
-  const handleContentChange = (content) => {
-    setShowContent(content);
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://backend-user-bms6.onrender.com/info-user/${username}`);
+        console.log(response.data);
+        setUserDetails(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (username) {
+      fetchData();
+    }
+  }, [username]);
+
+  // Check if user details are loaded before rendering content
+  if (!userDetails) {
+    return <div>Loading user details...</div>;
+  }
 
   const renderContent = () => {
     switch (showContent) {
       case 'loginSuccess':
         return (
           <div>
-            <h1>Login Success Page</h1>
+            <h2>User Details</h2>
+            <p>Username: {userDetails.username}</p>
+            <p>Email: {userDetails.email}</p>
+            <p>Name: {userDetails.name}</p>
+            <p>Gender: {userDetails.gender}</p>
+            <p>Phonenumber: {userDetails.phoneNumber}</p>
           </div>
         );
+
       case 'deleteUser':
         return (
           <div>
             <h1>Delete Doctor</h1>
           </div>
         );
+
       case 'updateUser':
-        return (
-          <div>
-            <h1>Edit Doctor</h1>
+         return (
+         <div>
+            <h1>Update Doctor</h1>
           </div>
         );
-      case 'infoUser':
-        return (
-          <div>
-            <h1>View Doctor Info</h1>
-          </div>
-        );
+
       default:
         return null;
     }
@@ -67,9 +89,6 @@ const ProfilePage = () => {
         </Link>
         <Link to='/updateuser' onClick={() => handleContentChange('updateUser')} style={buttonStyle}>
           Edit Doctor
-        </Link>
-        <Link to='/infouser' onClick={() => handleContentChange('infoUser')} style={buttonStyle}>
-          View Doctor Info
         </Link>
       </div>
       {renderContent()}
