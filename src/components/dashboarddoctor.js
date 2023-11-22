ce475e30677679ccbbb2fffc9a3a6220c2a46361
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-22 13:25:16 +0900

    Update dashboarddoctor.js

--- src/components/dashboarddoctor.js
+++ src/components/dashboarddoctor.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export function Navedoctor() {
   const [quoteIndex, setQuoteIndex] = useState(0);
   const [diseaseName, setDiseaseName] = useState('');
-const username = localStorage.getItem('username');
+
   const quotes = [
     "Your mental health is a priority. Don't forget to take care of yourself.",
   "Making a difference in someone's life is one of the greatest rewards there is. Embrace the power of your profession to help others heal and find hope.",
