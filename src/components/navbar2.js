05b91ca875897eabb80e66217bbb3a4fd10a568c
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-22 12:57:54 +0900

    Update navbar2.js

--- src/components/navbar2.js
+++ src/components/navbar2.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar2() {
+   const history = useHistory();
   const handleLogout = () => {
   localStorage.removeItem("username");
   history.push("/");
